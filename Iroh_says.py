5eed3a4821a0e4e74ae5c871c2e7ccb1af1517e9
Christopher Wong <31772094+xopherw@users.noreply.github.com>
2021-02-03 15:49:02 +0800

    Update Iroh_says.py

--- Iroh_says.py
+++ Iroh_says.py
@@ -1,5 +1,4 @@
 import discord,random, pathlib, pandas as pd, time, datetime as dt, lunarcalendar as lc
-from discord.ext import commands, tasks
 
 path = pathlib.Path('iroh.csv')
 iroh_quotes = pd.read_csv(path,header=None,sep='\n')[0].to_list()
@@ -7,19 +6,21 @@ iroh_quotes = pd.read_csv(path,header=None,sep='\n')[0].to_list()
 while(True):
     try: 
         client = discord.Client()
-        bot = commands.Bot(command_prefix='!')
 
-        @bot.command(name='iroh_says')
-        async def iroh_says(ctx):
-            response = random.choice(iroh_quotes)
-            await ctx.send(response)
+        @client.event
+        async def on_message(message):
+            if message.author == client.user:
+                return
+            
+            if message.content == '!roh':
+                response = random.choice(iroh_quotes)
+                await message.channel.send(response)
 
         @client.event
         async def on_ready():
             print(f'{client.user} has connected to Discord!')
-            # await job.start()
             while(True):
-                interval = 1
+                interval = 5
                 now = dt.datetime.now()
                 lunar = lc.Converter.Solar2Lunar(now)
                 if(lunar.day in [1,15] and now.hour == 0 and now.minute == 0): 
@@ -37,8 +38,10 @@ while(True):
         
         async def other_day(lunar):
             await client.wait_until_ready()
-            channel = await client.fetch_user(int(266031018295689226))
+
+            channel = await client.fetch_user(int('YOUR_USER_ID_WITHOUT_QUOTATION_MARK'))
             await channel.send(f'Today lunar date is month {lunar.month}, and day {lunar.day}.')
+            
 
         client.run('YOUR_TOKEN')
 
