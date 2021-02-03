67645d30bee3085643ff3deda4ac79c056a1ad25
Christopher Wong <31772094+xopherw@users.noreply.github.com>
2021-02-03 16:10:52 +0800

    Update Iroh_says.py

--- Iroh_says.py
+++ Iroh_says.py
@@ -1,4 +1,5 @@
 import discord,random, pathlib, pandas as pd, time, datetime as dt, lunarcalendar as lc
+from discord.ext import tasks
 
 path = pathlib.Path('iroh.csv')
 iroh_quotes = pd.read_csv(path,header=None,sep='\n')[0].to_list()
@@ -19,29 +20,27 @@ while(True):
         @client.event
         async def on_ready():
             print(f'{client.user} has connected to Discord!')
-            while(True):
-                interval = 5
-                now = dt.datetime.now()
-                lunar = lc.Converter.Solar2Lunar(now)
-                if(lunar.day in [1,15] and now.hour == 0 and now.minute == 0): 
-                    await vegan_day(lunar)
-                    interval = 2 * 60
-                elif(now.hour == 0 and now.minute == 0):
-                    await other_day(lunar)
-                    interval = 2 * 60
-                time.sleep(interval)
+            await isLunar.start()
+        
 
         async def vegan_day(lunar):
             await client.wait_until_ready()
-            channel = await client.fetch_user(int('YOUR_USER_ID_WITHOUT_QUOTATION_MARK'))
+            channel = await client.fetch_user(int("YOUR_ID_WITHOUT_QUOTATION_MARK"))
             await channel.send(f'Today is month {lunar.month} and day {lunar.day}. It\'s vegan day!')
         
         async def other_day(lunar):
             await client.wait_until_ready()
-
-            channel = await client.fetch_user(int('YOUR_USER_ID_WITHOUT_QUOTATION_MARK'))
+            channel = await client.fetch_user(int("YOUR_ID_WITHOUT_QUOTATION_MARK"))
             await channel.send(f'Today lunar date is month {lunar.month}, and day {lunar.day}.')
-            
+
+        @tasks.loop(minutes=1)
+        async def isLunar():
+            now = dt.datetime.now()
+            lunar = lc.Converter.Solar2Lunar(now)
+            if(lunar.day in [1,15] and now.hour == 0 and now.minute == 1): 
+                await vegan_day(lunar)
+            elif(now.hour == 0 and now.minute == 0):
+                await other_day(lunar)
 
         client.run('YOUR_TOKEN')
 
