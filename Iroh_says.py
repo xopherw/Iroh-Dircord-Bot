ff857c5c020d5c6642158a306679607485c55335
Christopher Wong <31772094+xopherw@users.noreply.github.com>
2021-02-06 11:02:42 +0800

    Update Iroh_says.py

--- Iroh_says.py
+++ Iroh_says.py
@@ -6,15 +6,15 @@ iroh_quotes = pd.read_csv(path,header=None,sep='\n')[0].to_list()
 
 client = discord.Client()
 
-async def vegan_day(lunar):
+async def vegan_day(lunar, now):
     await client.wait_until_ready()
     channel = await client.fetch_user(int('DISCORD USER ID WITHOUT QUOTATION MARK'))
-    await channel.send(f'Today is month {lunar.month} and day {lunar.day}. It\'s vegan day!')
+    await channel.send(f'Today is month {lunar.month} and day {lunar.day}. It\'s vegan day!\nGregorian date is month {now.month}, and day {now.day}.')
 
-async def other_day(lunar):
+async def other_day(lunar, now):
     await client.wait_until_ready()
     channel = await client.fetch_user(int('DISCORD USER ID WITHOUT QUOTATION MARK'))
-    await channel.send(f'Today lunar date is month {lunar.month}, and day {lunar.day}.')
+    await channel.send(f'Today lunar date is month {lunar.month}, and day {lunar.day}.\nGregorian date is month {now.month}, and day {now.day}.')
 
 @client.event
 async def on_message(message):
@@ -34,9 +34,9 @@ async def isLunar():
     now = dt.datetime.now()
     lunar = lc.Converter.Solar2Lunar(now)
     if(lunar.day in [1,15] and now.hour == 0 and now.minute == 1): 
-        await vegan_day(lunar)
+        await vegan_day(lunar, now)
     elif(now.hour == 0 and now.minute == 0):
-        await other_day(lunar)
+        await other_day(lunar, now)
 
 while(True):
     try: client.loop.run_until_complete(client.run('TOKEN'))
