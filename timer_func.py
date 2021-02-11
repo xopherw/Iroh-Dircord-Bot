21b1ecb6ac05df96d352b6e3deb2b900fec881c0
xopherw <xopher95@gmail.com>
2021-02-11 13:51:59 +0800

    help msg

--- timer_func.py
+++ timer_func.py
@@ -4,13 +4,27 @@ import json, asyncio
 #----------- constants -----------#
 
 help = """
-Welcome to my timer bot!!
-Use this `!timer start` command to start your countdown! By default, `!timer start` will only give you 15 minutes.\n
+Hello, I am General Iroh. You may also know me as the Dragon of the West. No? It's more of demonstration really. 
+But this is not why I am here for. I am a utility tool for you. Here I have many functions like quoting all my Avatar quotes, set a timer, and rolling dices.
+
+To use the Pomodoro timer, you should use the `!roh timer` command. By default, `!roh timer` will only give you 15 minutes.
 However, you can make your own timer from 0 to 99 mintues using command like this:
 `!timer start 40` for 40 minutes timer.\n
 When the timer is up, it will nudge you every 5 seconds. To stop that, just reply `!done` and it will stop.
-Furthermore, you can use `!stop` command to stop the timer in the middle of countdown, and timer will be reset.\n
-I hope you find this useful. Have fun! 
+Furthermore, you can use `!stop` command to stop the timer in the middle of countdown, and timer will be reset.
+
+If you like to brighten up your day with a quote, you may use the `!iroh` command. I will reply you a random quote from the Avatar show. 
+
+If you have interest in dice rolling, you can use my `!roll` command too! 
+However to roll a dice, you need to mention numbers of dice, and numbers of sides on the dice. 
+Let me show you:
+`!roll 1d10` will generate one random number between 1 to 10.
+`!roll 3d10` will generate three random numbers between 1 to 10, and will also show you the sum of the three dices added up.
+
+There will be more functions to be added as my maker is gathering idea for me. So I hope you found some of my functions helpful!
+
+When I'm not doing any task mentioned, I'll go make myself some tea...would you like one? 
+Sharing tea with a fascinating stranger is one of life's true delights. :tea:
 """
 
 #----------- async defs -----------#
@@ -20,7 +34,7 @@ async def userCheck(client, period, message):
     if(json.loads(open('user.json' , 'r').read()).get(str(message.author.id)) != None): 
         await sender(message, "Only one timer is allowed. Stop & reset if necessary.")  
     else:
-        await sender(message, f"You have set a timer for {period} minutes! Now go get them tiger!" if(period != 15) else "No time set, default timer is 15 minutes! Now go get them tiger!") 
+        await sender(message, f"{period} minutes set! Focus on your element bend!" if(period != 15) else "No time set, default timer is 15 minutes! Focus on your element bend!") 
         await timer(client, period, message) 
 
 # Does the countdown work as well as remove user once "stop" command is used, or "done" command when time is up.
@@ -39,8 +53,8 @@ async def timer(client, period, message):
             try: 
                 await sender(message, 'Times Up!!!')
                 def done(m): return m.content == '!done' and m.author.id == message.author.id
-                await waiter(client, 2.5, done)
-                await sender(message, 'Looks like you\'v completed your training! Go reward yourself a nice warm tea.')
+                await waiter(client, 5, done)
+                await sender(message, 'Looks like you\'ve completed your training! Go reward yourself a nice warm tea.')
                 removeID(message.author.id)
                 break
             except asyncio.TimeoutError:
