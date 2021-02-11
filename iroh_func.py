166e72ac8ff3ff45cdbc14fe4aa42d4ea98705b2
xopherw <xopher95@gmail.com>
2021-02-11 14:38:31 +0800

    update on help with embed

--- iroh_func.py
+++ iroh_func.py
@@ -10,24 +10,22 @@ No? It's more of demonstration really. But this is not why I am here for.
 I am here to help you like how I helped my favorite nephew Zuko.
 
 
-**--------- !roh command: **
+**!roh command: **
 This command will return a random quote I have said from the Avatar show.
 
-**--------- !roh timer command: **
-To use the Pomodoro timer, you should use the `!roh timer` command. By default, `!roh timer` will only give you 15 minutes.
+
+**!roh timer command: **
+By default, `!roh timer` will only give you 15 minutes.
 
 However, you can make your own timer from 0 to 99 mintues using command like this:
-
 `!roh timer 40` for 40 minutes timer.
 
-When the timer is up, I will nudge you (in personal message) every 5 seconds. 
-
-Just reply `!done` and it will stop. 
+When the timer is up, I will nudge you (in personal message) every 5 seconds. Just reply `!done` and it will stop. 
 
 You can also use `!stop` command to stop the timer in the middle of countdown, and timer will be reset.
 
 
-**--------- !roll [number X]d[number Y] command: **
+**!roll [number X]d[number Y] command: **
 This command will roll a dice for any RPG event or you lost your dice in your board game.
 
 Let me show you how to use the command:
@@ -35,11 +33,11 @@ Let me show you how to use the command:
 `!roll 1d10` will generate one random number between 1 to 10
 returns: `@usernamme rolls 1d10 and gets 11.`
 
-`!roll 3d10` will generate three random numbers between 1 to 10, and will also show you the sum of the three dices added up.
+`!roll 3d10` will generate three random numbers between 1 to 10, and will also retrun the sum of the three dices numbers.
 returns: `@username rolls 3d20 and gets 24. [4, 10, 10]`
 
 
-**--------- More **
+**More **
 There will be more functions to be added as my maker is gathering new idea for me. 
 So I hope you found some of my functions helpful!
 
