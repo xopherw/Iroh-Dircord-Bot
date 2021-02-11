c330e840af0571a043b1d5aa616e02ae84dfd51e
xopherw <xopher95@gmail.com>
2021-02-11 13:13:18 +0800

    mention user

--- Iroh_says.py
+++ Iroh_says.py
@@ -19,9 +19,10 @@ async def on_message(message):
     if (re.match(r"^(\!roll) [0-9]{1,3}d[0-9]{1,3}$", message.content)):
         roll = [int(i) for i in message.content.split(' ')[-1].split('d')]
         result = [random.randint(1,roll[-1]) for i in range(roll[0])]
-        msg1 = f"{message.author} rolls {roll[0]}d{roll[-1]} and gets {sum(result)}. {result if(len(result) > 1) else ''}"
-        msg2 = f"{message.author} rolls {roll[0]}d{roll[-1]} and gets {sum(result)}."
-        await message.channel.send(f"{ msg1 if(len(msg1) < 2000) else  msg2 }  ")
+        id = message.author.id
+        msg1 = f"<@{id}> rolls {roll[0]}d{roll[-1]} and gets {sum(result)}."
+        msg2 = f"<@{id}> rolls {roll[0]}d{roll[-1]} and gets {sum(result)}. " + f"{result if(len(result) > 1) else ''}"
+        await message.channel.send(f"{ msg1 if(len(msg1) > 2000) else  msg2 }  ")
 
 @client.event
 async def on_ready():
